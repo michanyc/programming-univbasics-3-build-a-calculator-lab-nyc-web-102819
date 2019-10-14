f32cdd958c13b135c7b28c6cbf3b8f3e0ecedb46
Michael Gerland <mr.gerland@gmail.com>
2019-10-14 23:01:49 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -9,16 +9,19 @@ end
 subtraction("10", "5")
 
 def division(num1, num2)
-
+  num1 / num2
 end
+division("50", "2")
 
 def multiplication(num1, num2)
-
+  num1 * num2
 end
+multiplication("4", "30")
 
 def modulo(num1, num2)
-
+  num1 % num2
 end
+modulo("34", "5")
 
 def square_root(num)
 
